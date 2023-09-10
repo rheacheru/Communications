d4361007e1d87dac36a997b0aa4a0af04d015669
Sey Kolla <shreyakolla@gmail.com>
2023-09-10 11:48:47 -0700

    basic sending code

---  comms.py
+++  comms.py
@@ -1,23 +1,30 @@
-#import pycubed_rfm9x
+from pysquared import cubesat as c
+import functions 
 import time
 import os
 
-
+f = functions.functions(c)
 def send_image(filepath):
     size = os.path.getsize(filepath)
     #password = 13
-    packet = size.to_bytes(4, byteorder = "big")
-    pass
+    packet = size.to_bytes(6, byteorder = "big")
+    packet = bytearray(packet)
+    packet[0] = 13 #password
+    packet[1] = 1 #indicator for images
+    f.send(packet)
+    with open(filepath, "rb") as stream:
+        while True:
+            data = stream.read(252)
+            if not data:
+                break
+            f.send(data)
+            time.sleep(2)
+
+send_image(filepath="image.jpg")
 
 
-size = 234233211
-p = 13
+    
 
-packet = size.to_bytes(6, byteorder="big")
-password = p.to_bytes(1, byteorder="big")
 
-packet = bytearray(packet)
-packet[0] = 13
-packet[1] = 1
-for i in range(6):
-    print(packet[i])
+
+
