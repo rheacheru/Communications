358a5dd853bd75f660d577457b53128c9840a406
Sey Kolla <shreyakolla@gmail.com>
2023-12-29 11:46:52 -0800

    it works :)

--- CIRCUITPY/code.py
+++ CIRCUITPY/code.py
@@ -11,6 +11,7 @@ import adafruit_requests
 import rtc
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 import ssl
+from datetime import datetime
 
 CS = digitalio.DigitalInOut(board.D12)
 CS.switch_to_output(True)
@@ -36,25 +37,84 @@ def main1():
 
         
 
+def attempt_wifi():
+    # TODO: Move wifi pass and id to config
+    # try connecting to wifi
+    print("Connecting to WiFi...")
+    try:
+        wifi.radio.connect(ssid=secrets["wifi"]["ssid"], password=secrets["wifi"]["password"])
+        # wifi.radio.connect(ssid="Stanford") # open network
+        print("Signal: {}".format(wifi.radio.ap_info.rssi))
+        # Create a socket pool
+        pool = socketpool.SocketPool(wifi.radio)
+        # sync out RTC from the web
+        synctime(pool)
+    except Exception as e:
+        print("Unable to connect to WiFi: {}".format(e))
+        return None
+    else:
+        return pool    
         
+def synctime(pool):
+    try:
+        requests = adafruit_requests.Session(pool)
+        TIME_API = "http://worldtimeapi.org/api/ip"
+        the_rtc = rtc.RTC()
+        response = None
+        while True:
+            try:
+                print("Fetching time")
+                # print("Fetching json from", TIME_API)
+                response = requests.get(TIME_API)
+                break
+            except (ValueError, RuntimeError) as e:
+                print("Failed to get data, retrying\n", e)
+                continue
+
+        json1 = response.json()
+        print(json1)
+        current_time = json1['datetime']
+        the_date, the_time = current_time.split('T')
+        year, month, mday = [int(x) for x in the_date.split('-')]
+        the_time = the_time.split('.')[0]
+        hours, minutes, seconds = [int(x) for x in the_time.split(':')]
+
+        # We can also fill in these extra nice things
+        year_day = json1['day_of_year']
+        week_day = json1['day_of_week']
+        is_dst = json1['dst']
+
+        now = time.struct_time(
+            (year, month, mday, hours, minutes, seconds, week_day, year_day, is_dst))
+        the_rtc.datetime = now
+    except Exception as e:
+        print('[WARNING]', e)
+
+
 
 def main2():
+    pool = attempt_wifi()
     print('in main2')
     while True:
         packet = rfm9x.receive(timeout=10)
         if packet is not None:
-            current_time = time.time()
+            now = datetime.now()
+            current_time = now.strftime("%Y%M%D%H%M%S")
             path =f'received_images/{current_time}.jpg'
+            print(path)
             size = int.from_bytes(packet, 'big')
             print(size)
             with open(path, "wb+") as stream:
                 count = 0
+                rssi = 0
                 while count < size:
                     data = rfm9x.receive(timeout=10)
+                    rssi = rssi + rfm9x.last_rssi
                     stream.write(data)
                     count = count + 249
                     print('done')
             print('saved image')
+            print(f'avg rssi: {rssi//((count//249)+1)}')
 
 
 main2()
