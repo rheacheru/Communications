f476dede7040de51d449754e4a23b94aef3be339
Sey Kolla <shreyakolla@gmail.com>
2024-01-12 23:02:49 -0800

    changed format

--- imu.py
+++ imu.py
@@ -4,11 +4,13 @@ from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 from picamera2 import Picamera2 #need to install?
 import time
 import math
+from datetime import datetime
 
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor1 = LSM6DSOX(i2c)
 camera = Picamera2()
+THRESHOLD = 18
 max = 0
 
 def main():
@@ -18,9 +20,9 @@ while True:
     accelx, accely, accelz = sensor1.acceleration
     accelz = accelz - 9.8
     accel_value = math.sqrt(math.pow(accelx, 2)+ math.pow(accely, 2)+ math.pow(accelz, 2))
-    print(f'accel: {accel_value}, max: {max}')
-    if accel_value > max:
-        max = accel_value
+    if(accel_value > THRESHOLD):
+        time = datetime.now().strftime("%Y%M%D%H%M%S")
+        camera.capture_file('test.png')
     time.sleep(1)
 
 
